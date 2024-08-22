2c432709968f3d4bd95e915a8e4473cfe044b97e
Fawaz Ogunleye <165667474+FazzytheDev@users.noreply.github.com>
2024-08-22 01:46:50 +0100

    fafa

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.post('/auth/telegram', async(req, res) => {
+    const user = req.body.user;
+    if(user && user.id){
+        res.render('dashboard', {
+            telegramId: user.id,
+            username: user.username,
+            referralId: user.referralId,
+            balance: 1000
+        });
+    }
+    else{
+        res.render('/error', {
+            error: 'not found'
+        });
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('server started at port xoxo');
+})
