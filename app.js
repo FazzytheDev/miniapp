ba9de26aea95397bcb73ce072f72abe9eb739806
Fawaz Ogunleye <165667474+FazzytheDev@users.noreply.github.com>
2024-08-23 10:27:31 +0100

    send

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
-const querystring = require('querystring');
-const ejs = require('ejs');
 const bodyParser = require('body-parser');
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
+
+// Set view engine to EJS
 app.set('view engine', 'ejs');
 
-// Route to handle verification (now just directly rendering the dashboard)
-app.get('/', (req, res) =>{
-    res.render('index');
+// Route to serve the initial EJS file
+app.get('/', (req, res) => {
+    res.render('webapp');
 });
+
+// Route to receive the Telegram data and render it on the dashboard
+app.post('/send-telegram-data', (req, res) => {
+    const { user } = req.body;
+
+    // Render the dashboard with the received data
+    res.render('dashboard', { 
+        telegramId: user.id,
+        firstName: user.first_name,
+        lastName: user.last_name || 'N/A',
+        username: user.username || 'N/A',
+        languageCode: user.language_code || 'N/A'
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
