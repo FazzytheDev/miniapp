8b3c5c31d46ce93bf94027f252ce662ec8066514
Fawaz Ogunleye <165667474+FazzytheDev@users.noreply.github.com>
2024-08-23 10:03:03 +0100

    send

--- app.js
+++ app.js
@@ -8,28 +8,9 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 // Route to handle verification (now just directly rendering the dashboard)
-app.get('/verify', (req, res) => {
-    const initData = req.query.init_data;
-
-    if (initData) {
-        const user = querystring.parse(initData);
-        res.redirect(`/dashboard?telegramId=${user.id}`);
-    } else {
-        res.status(400).send('Invalid data');
-    }
+app.get('/', (req, res) =>{
+    res.render('index');
 });
-
-// Route to handle dashboard display
-app.get('/dashboard', (req, res) => {
-    const telegramId = req.query.telegramId;
-    if (telegramId) {
-        // Render the dashboard with the Telegram ID
-        res.render('dashboard', { telegramId: telegramId });
-    } else {
-        res.status(400).send('Telegram ID is missing');
-    }
-});
-
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
